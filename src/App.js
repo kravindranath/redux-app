26af34792a409aacce9662ed3a8b67d50cd3a926
Ravindranath K <ravindranath.k@gmail.com>
2020-12-01 14:35:24 +0530

    make counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  function incrementCount(){
+    setState((prevState)=>{
+      return prevState + 1;
+    })
+  }
+
+  function decrementCount(){
+    setState((prevState)=>{
+      return prevState  - 1;
+    })
+  }
+
+  const [ count, setState ] = useState(1);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="msg">You clicked {count} times </div>
+      <button className="button plus" type="button" onClick={ () => incrementCount() }>
+        +
+      </button>
+      <button className="button minus" type="button" onClick={ () => decrementCount() }>
+        -
+      </button>
     </div>
   );
 }
